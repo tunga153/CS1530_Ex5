a17b7dd957b3274653e53377516e821663869bb9
Sam Nosenzo <samnosenzo@gmail.com>
2017-11-15 15:59:02 -0500

    push

--- Ex5.java
+++ Ex5.java
@@ -3,19 +3,47 @@ import java.util.*;
 public class Ex5 {
 	public static int[] sums;
 	public static void main(String[] args) {
-			int numThreads = -1;
-			int numIterations = -1;	
-			int iterationsPerThread = -1;
-			try {
-				numThreads = Integer.parseInt(args[0]);
-				numIterations = Integer.parseInt(args[1]);
-			} catch (Exception e) {
-				System.exit(23523535);
+		int numThreads = -1;
+		int numIterations = -1;	
+
+		try {
+			numThreads = Integer.parseInt(args[0]);
+			numIterations = Integer.parseInt(args[1]);
+		} catch (Exception e) {
+			System.exit(23523535);
+		}
+		sums = new int[numThreads];
+		Arrays.fill(sums, 0);
+		final int iterationsPerThread = numIterations/numThreads;
+		Thread[] threads = new Thread[numThreads];
+		for(int i = 0; i < numThreads; i++ ){
+			final int ithread = i;
+			threads[i] = new Thread(() -> {
+				monteCarlo(ithread, iterationsPerThread);
+			});
+		}
+		try {
+			for(Thread t: threads) {
+				t.start();
 			}
-			sums = new int[numThreads];
-			Arrays.fill(sums, 0);
-			iterationsPerThread = numIterations/numThreads;
-			Thread[] Threads = new Thread[numThreads];
+
+			for(Thread t: threads) {
+				t.join();
+			}
+		}
+
+		int totalSum = 0;
+		for(int i = 0; i < numThreads; i++) {
+			totalSum += sums[i];
+		}
+
+		System.out.println("Total: " + numIterations);
+		System.out.println("Total within radius: " + totalSum);
+		double ratio = (((double) totalSum)/((double) numIterations);
+		System.out.println("Ratio: " + ratio);
+		System.out.println("PI = " + (ratio * 4)) ;
+
+
 
 	}
 
